b90b2012cd21718501efd4771664a47188344319
Biswajit Das <workbiswajit08981@gmail.com>
2025-05-11 12:24:07 +0530

    fix not found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -6,7 +6,7 @@ const NotFound = () => {
         <div className="h-full w-full relative  flex-center">
             <div className="flex flex-col items-center gap-4">
                 <h1 className="tex-7xl font-bold">404 something went wrong</h1>
-                <Link href={"/auth/login"} className="bg-orange-600
+                <Link href={"/"} className="bg-orange-600
                 px-5 py-2 rounded-md"> Retry </Link>
             </div>
         </div>
